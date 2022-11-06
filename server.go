30751520334f927750d2e00ac65c72efc8ed9859
Mark Chang <mark.chang@vpon.com>
2022-11-06 13:57:36 +0800

    add auth module

--- server.go
+++ server.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/mark1002/graphsql-pratice/graph"
 	"github.com/mark1002/graphsql-pratice/graph/generated"
+	"github.com/mark1002/graphsql-pratice/internal/auth"
 	database "github.com/mark1002/graphsql-pratice/internal/pkg/db/mysql"
 )
 
@@ -19,14 +21,16 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+	router := chi.NewRouter()
+	router.Use(auth.Middleware())
 	database.InitDB()
 	defer database.CloseDB()
 	database.Migrate()
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
